0c9d275e6b7e2bd2a7d7921c89648818ee859985
Bilal Özkırış <bilalozkiris@gmail.com>
2021-07-18 02:18:27 +0300

    program that calculates element frequencies of an array added

---
+++ Java101/FrequencyArray/src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int findIndex(int[][] arr, int value) {
+        /*
+         int idx = findIndex(arr, value);
+
+            INPUTS;
+                arr: two dimensional array
+                value: int number
+            OUTPUT;
+                index of the value in first column of the arr.
+         */
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i][0] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isIn(int[][] arr, int value) {
+        /*
+        boolean isIn = isIn(arr, value);
+
+            INPUTS;
+                arr: two dimensional array
+                value: int number
+            OUTPUT;
+                Boolean value that corresponding to occurrence of value in arr
+         */
+        for (int[] row : arr) {
+            if (row[0] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[][] elementFrequenciesInAnArray(int[] arr) {
+        /*
+        int[][] freq = elementFrequenciesInAnArray(arr);
+
+            INPUT;
+                arr: one dimensional array
+            OUTPUT;
+                two dimensional array with frequency values of elements in arr
+         */
+        int[][] frequencies = new int[arr.length][2];
+        for (int i = 0; i < arr.length; i++) {
+            if (!isIn(frequencies, arr[i])) {
+                frequencies[i][0] = arr[i];
+                frequencies[i][1] = 1;
+            } else {
+                frequencies[findIndex(frequencies, arr[i])][1] += 1;
+            }
+        }
+        return frequencies;
+    }
+
+    public static void displayFrequencies(int[][] arr) {
+
+        for (int[] row : arr) {
+            if (row[1] > 0) {
+                System.out.println("The number " + row[0] + " was repeated " + row[1] + " times.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] list = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1, 1, 0};
+        int[][] freqArr = elementFrequenciesInAnArray(list);
+
+        System.out.println("Array: " + Arrays.toString(list));
+        System.out.println("Frequencies;");
+        displayFrequencies(freqArr);
+
+    }
+}
