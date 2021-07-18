1353146414a2ff1982540bb25d3ea4777b1ab0da
Bilal Özkırış <bilalozkiris@gmail.com>
2021-07-18 20:56:11 +0300

    program that finds out if a word palindrome or not added

--- Java101/PalindromeWord/src/Main.java
+++ Java101/PalindromeWord/src/Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isPalindrome(String s) {
-        int counter = 0;
         for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) == s.charAt(s.length() - (i + 1))) {
-                counter++;
+            if (s.charAt(i) != s.charAt(s.length() - (i + 1))) {
+                return false;
             }
         }
-        return counter == s.length() / 2;
+        return true;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
